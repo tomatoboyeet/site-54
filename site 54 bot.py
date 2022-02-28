60cbf3f6621a4e52a8076f8ccb8cefe2e7bb33bc
tomatoboyeet <93995829+tomatoboyeet@users.noreply.github.com>
2022-02-28 11:12:07 +0200

    Update site 54 bot.py

--- site 54 bot.py
+++ site 54 bot.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord.ext.commands import cooldown,BucketType,MemberConverter
 from discord import User
 from math import *
+from threading import *
 from datetime import datetime
 from discord.client import Client#0163 7934
 def startswith(self,data):return self.removeprefix(data)==self#2494 6903#4619 5165
@@ -10,7 +11,7 @@ bot=commands.Bot(command_prefix="sb ")#3205 6416
 @bot.event
 async def on_ready():print('ready')
 @bot.event
-async def on_member_join(member):await int(926557725188571166).send(f'{str(member.user)}  had joined')
+async def on_member_join(member):print(f'{str(member.user)}  had joined')
 @bot.event#8095 5306
 async def on_message(msg):#8027 5090
     if not msg.author.id==930000443017429032:#4361 3746
@@ -29,12 +30,17 @@ async def on_message(msg):#8027 5090
                     for i in msg.content.removeprefix('sb convert bin '):result+=bin(ord(i))
                     await msg.channel.send(result.replace('0b',''))
             elif msg.content.lower().startswith('sb calc '):
+                
                 if'exit'in msg.content or'guilds'in msg.content or'exec'in msg.content or'open'in msg.content or'close'in msg.content or'eval'in msg.content:await msg.channel.send('dont try injecting me retard')
                 elif msg.content.lower().startswith('sb calc 10+9')or msg.content.lower().startswith('sb calc 9+10'):await msg.channel.send('21')
                 else:
                     try:
                         x=eval(msg.content.removeprefix('sb calc ').removeprefix('Sb calc ').replace('^','**'))
-                        if'nigger'in x or'faggot'in x or'nigga'in x:await msg.channel.send('mf tries making me say shit')
+                        if'nigger'in str(x) or'faggot'in str(x) or'nigga'in str(x):await msg.channel.send('mf tries making me say shit')
                         else:await msg.channel.send(x)
                     except Exception as e:await msg.channel.send(e)
+@bot.event
+async def on_reaction_add(reaction,user):
+    if reaction=='🎩':await bot.add_roles(user.id,discord.utils.get(server.roles,name="alpha boy🎩"))
+    elif reaction=='👒':await bot.add_roles(user.id,discord.utils.get(server.roles,name='Material girl👒'))
 bot.run('OTMwMDAwNDQzMDE3NDI5MDMy.YdvgaA.P1EMZkXmpfc_nVYBwMBqB5z9iPw')
